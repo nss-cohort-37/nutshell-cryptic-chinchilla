48ed91b1a531ebd718f77473e2ca0c3ffc7b4f20
wggreen <wggreen.nss@gmail.com>
2020-01-15 15:04:22 -0600

    updated provider with expand to get users

---
+++ src/scripts/messages/Messages.js
@@ -0,0 +1,12 @@
+const MessageComponent = (username, message) => {
+    
+    return `
+        <section class="message">
+            <div class="message_username">
+            <dialog class="" id="">
+            </dialog>
+        </section>
+    `
+}
+
+export default CriminalComponent
--- src/scripts/messages/MessagesProvider.js
+++ src/scripts/messages/MessagesProvider.js
@@ -2,7 +2,7 @@ let messages = []
 
 export const useMessages = () => messages.slice()
 
-export const getMessages = () => fetch("http://localhost:3000/messages")
+export const getMessages = () => fetch("http://localhost:3000/messages?_expand=user")
     .then(res => res.json())
     .then(paresedMessages => messages = paresedMessages)
 
