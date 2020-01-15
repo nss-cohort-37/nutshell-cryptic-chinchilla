b4259d4737c6a2b1bb969d3384b0a54350dc1659
wggreen <wggreen.nss@gmail.com>
2020-01-15 15:33:46 -0600

    finished message component

--- src/scripts/messages/Messages.js
+++ src/scripts/messages/Messages.js
@@ -2,11 +2,23 @@ const MessageComponent = (message) => {
     
     return `
         <section class="message">
-            <div class="message_username">
-                ${message.user.name}
+            <div class="messageUsername">
+                ${message.user.username}: 
+            </div>
+            <div class="messageContent>
+                ${message.message}
+            </div>
+            <div class="messageEditButton">
+                <button id="editMessage--${message.id}">
+                    Edit Message
+                </button>
             </div>
 
-            <dialog class="" id="">
+            <dialog class="messageDialog" id="messageEditDialog">
+                <input type="text" id="messageText--${message.id}" value="${message.message}"></input>
+                <button id="saveEdit--${message.id}">
+                    Save Edit
+                </button>
             </dialog>
         </section>
     `
