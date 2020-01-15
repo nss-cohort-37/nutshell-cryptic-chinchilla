8a57134245261024325c499dcab6dab024104b0d
wggreen <wggreen.nss@gmail.com>
2020-01-15 15:08:09 -0600

    just want to merge

---
+++ src/scripts/messages/Messages.js
@@ -0,0 +1,15 @@
+const MessageComponent = (message) => {
+    
+    return `
+        <section class="message">
+            <div class="message_username">
+                ${message.user.name}
+            </div>
+
+            <dialog class="" id="">
+            </dialog>
+        </section>
+    `
+}
+
+export default CriminalComponent
