f092aeb81203fcc04f9f9133bf97175fc8c9f9d1
aborgra <audrey.borgra@gmail.com>
2020-01-16 16:03:48 -0600

    navbar logOut works

--- src/scripts/messagesMain.js
+++ src/scripts/messagesMain.js
@@ -1,5 +1,7 @@
 import { getMessages } from "./messages/MessagesProvider.js"
 import { MessageList } from "./messages/MessageList.js"
+import { MessagesNavbarEventListener } from "./navbar/navbarList.js"
 
+MessagesNavbarEventListener()
 getMessages()
     .then(() => MessageList())
