ba9b4570f075e47a002f833e786b0818d1e7da54
wggreen <wggreen.nss@gmail.com>
2020-01-16 17:47:44 -0600

    refactored login page

--- src/scripts/logInMain.js
+++ src/scripts/logInMain.js
@@ -1,9 +1,9 @@
-import { logInList } from "./logIn/LogInList.js";
-import { logInEvent } from "./logIn/LogInForm.js";
-import { getUsers } from "./users/UsersProvider.js";
+// import { logInList } from "./logIn/LogInList.js";
+// import { logInEvent } from "./logIn/LogInForm.js";
+// import { getUsers } from "./users/UsersProvider.js";
 
-getUsers().then( () => {
-  logInList()
-  logInEvent()
-}
-)
+// getUsers().then( () => {
+//   logInList()
+//   logInEvent()
+// }
+// )
---
+++ src/scripts/main.js
@@ -0,0 +1,8 @@
+import { logInList } from "./logIn/LogInList.js";
+import { logInEvent } from "./logIn/LogInForm.js";
+import { getUsers } from "./users/UsersProvider.js";
+
+getUsers().then(() => {
+    logInList()
+    logInEvent()    
+})
