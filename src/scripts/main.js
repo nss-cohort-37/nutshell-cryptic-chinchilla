d4554062598885bd11efc7d31d1ed3d1ae65e188
wggreen <wggreen.nss@gmail.com>
2020-01-17 09:47:01 -0600

    more work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,11 @@
 import { getEvents } from "./events/EventProvider.js"
 import { getUsers } from "./users/UsersProvider.js"
 import { EventList } from "./events/EventList.js"
+import { NavbarEventListener } from "./navbar/navbarList.js"
 
 getUsers()
 
 getEvents()
     .then(() => EventList())
-import { NavbarEventListener } from "./navbar/navbarList.js";
 
 NavbarEventListener()
