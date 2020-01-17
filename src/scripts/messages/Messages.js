0ff47ca8eb737cc111208848f3db171b10772c8b
wggreen <wggreen.nss@gmail.com>
2020-01-17 11:05:12 -0600

    commit

--- src/scripts/messages/Messages.js
+++ src/scripts/messages/Messages.js
@@ -22,13 +22,3 @@ export const MessageComponent = (message) => {
         </section>
     `
 }
-
-export const showEditButton = () => {
-    if (message.userId === parseInt(sessionStorage.getItem("activeUser), 10)) {
-    let contentTarget = document.querySelector(".editMessageButton")
-    contentTarget.innerHTML = `
-        <button id="saveEdit--${message.id}">
-                Save Edit
-        </button>
-    `
-}
