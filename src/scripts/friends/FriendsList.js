7a142f6ca74082746e08bfe66b13c49060fec9bd
aborgra <audrey.borgra@gmail.com>
2020-01-17 10:13:39 -0600

    friends list renders and delete friend button works with a re render of the friends list

--- src/scripts/friends/FriendsList.js
+++ src/scripts/friends/FriendsList.js
@@ -1,12 +1,34 @@
-import { FriendCard } from "./FriendCard.js"
-import { useFriends } from "./FriendsProvider.js"
+import { FriendCard } from "./FriendCard.js";
+import { useFriends } from "./FriendsProvider.js";
 
-const eventHub = document.querySelector(".container")
+const eventHub = document.querySelector(".container");
+const contentElement = document.querySelector(".friendsContainer");
 
-export const FriendsList = () => {
+export const FriendsListComponent = () => {
+  const allFriends = useFriends();
+  const activeUserId = parseInt(sessionStorage.getItem("activeUser"), 10);
+  console.log("activeUserId");
+  const foundFriendsArray = allFriends.filter(
+    friendRel => friendRel.friendInitiateId === activeUserId
+  );
 
-const friends = useFriends()
-const activeUserId = parseInt(sessionStorage.getItem("activeUser"), 10)
-console.log("activeUserId")
+  render(foundFriendsArray);
 
-}
+  eventHub.addEventListener("friendDeleted", () => {
+    console.log("deleted friend event heard")
+    debugger
+    const updatedFriends = useFriends()
+    const activeUserId = parseInt(sessionStorage.getItem("activeUser"), 10);
+    const updatedFoundFriendsArray = updatedFriends.filter(friendRel => friendRel.friendInitiateId === activeUserId)
+    contentElement.innerHTML = ""
+    render(updatedFoundFriendsArray)
+  });
+};
+
+export const render = foundFriendsArray => {
+  contentElement.innerHTML = `
+  ${foundFriendsArray.map(foundFriend => {
+    return FriendCard(foundFriend);
+  }).join("")}
+  `;
+};
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 import { NavbarEventListener } from "./navbar/navbarList.js";
+import { FriendsListComponent } from "./friends/FriendsList.js";
+import { getFriends } from "./friends/FriendsProvider.js";
 
 NavbarEventListener()
+getFriends().then(
+FriendsListComponent)
